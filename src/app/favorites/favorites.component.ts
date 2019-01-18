7a9919c5efe7b7b1913ad88e1330ac6427b513d7
Alex <reyesgx@gmail.om>
2019-01-18 13:53:46 -0800

    successfully placed id, symbol, and  price and same obj => do it w/o find()

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -22,39 +22,24 @@ export class FavoritesComponent implements OnInit {
     this.getFav();
   };
 
-  favoriteList: any = [];
+  favoriteList: any[] = [];
   favoriteData: any = {};
-  stockArray: any = [];
+  stockArray: any[] = [];
   stockData: any = {};
   
 // step 2) for each index send the ticker symbol to obtain an obj with the price and stock symbol (metadata), place those 2 in an obj and then push them in an array.
 // (side note: the ticker symbol is send in the order they appear in the 'stockArray', but are not received in order(FavoriteList), therefore, it is not easy to link price, symbol and id in the same obj in order)
-  putPriceinStockData() {
-    let dog = this.favoriteList.find((element, i) => {
-      console.log(element.symbol, i, this.stockArray[i].symbol )
-      return element.symbol == this.stockArray[i].symbol;
-    });
-    console.log(dog)
-  }  
 
   getIntraPrice(symbolArray: any) {
     symbolArray.map( index => {
       this._stock.serviceIntraDay(index.symbol)
-        .subscribe( response => {
-          console.log(response, "#2")
-          this.favoriteData.symbol = response['Meta Data']['2. Symbol']
-          let priceKey = Object.keys(response["Time Series (15min)"])[0]
-          //this.favoriteData.price = response["Time Series (15min)"][priceKey]["4. close"]
-          console.log(index.symbol, '#3')
-          let dog = this.stockArray.find( each => {
+        .subscribe( response => {         
+          let priceKey = Object.keys(response["Time Series (15min)"])[0];
+          let objData = symbolArray.find( each => {
             return each.symbol == index.symbol
-          })
-          dog.price = response["Time Series (15min)"][priceKey]["4. close"] 
-          console.log(dog, '#4')
-          // this.favoriteList.push(this.favoriteData)
-          // this.favoriteData = {}
-          console.log(this.favoriteList, "2.2")
-          //this.putPriceinStockData();
+          });
+          objData.price = response["Time Series (15min)"][priceKey]["4. close"]; 
+          this.favoriteList.push(objData);
         })
     })
   }
@@ -62,14 +47,6 @@ export class FavoritesComponent implements OnInit {
   getFav() {
     this._user.getFavoritesData(window.sessionStorage.getItem('token'), window.sessionStorage.getItem('userId'))
     .subscribe((response: any) => {
-      console.log(response, "#3.1")
-      response.forEach( each => {
-        this.stockData.id = each.id;
-        this.stockData.symbol = each.symbol;
-        this.stockArray.push(this.stockData)
-        this.stockData = {}
-      })
-      console.log(this.stockArray, '#3.2')
       this.getIntraPrice(response);
     })  
   }
@@ -78,7 +55,7 @@ export class FavoritesComponent implements OnInit {
   addFavorite() {
     let list = {
       symbol: ''
-    }
+    };
     list.symbol = this._stock.stockSymbol;
     this._user.saveFavorite(list, window.sessionStorage.getItem('token'), window.sessionStorage.getItem('userId'))
       .subscribe((response: any) => {
@@ -108,8 +85,8 @@ export class FavoritesComponent implements OnInit {
       })
   }
 
-  onStockClick(symbol) {
-    console.log("clicked")
+  onDeleteStock(symbol) {
+    console.log("clicked", symbol)
   }
 
   searchStock() {
